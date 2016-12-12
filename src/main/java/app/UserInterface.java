7582ab8731a7cf7247dcdef47c1647664727d824
VZ <valentinazelayam@gmail.com>
2016-12-12 13:18:20 +0100

    Added search bar and little license panel

--- src/main/java/app/UserInterface.java
+++ src/main/java/app/UserInterface.java
@@ -12,16 +12,22 @@ import main.java.views.*;
 public class UserInterface extends JFrame {
 	
 	CentralView center;
+	TopView top;
+	BottomView bottom;
 	
 	JPanel mainPanel;
 	
 	public UserInterface(){
 		super("A Youtube Copycat");
 		center=new CentralView();
+		top=new TopView();
+		bottom=new BottomView();
 		mainPanel=new JPanel();
 		this.setPreferredSize(new Dimension(1500,1000));
 		mainPanel.setLayout(new BorderLayout());
 		mainPanel.add(center,BorderLayout.CENTER);
+		mainPanel.add(top,BorderLayout.NORTH);
+		mainPanel.add(bottom,BorderLayout.SOUTH);
 		this.add(mainPanel);
 
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
--- src/main/java/views/TopView.java
+++ src/main/java/views/TopView.java
@@ -1,5 +1,12 @@
 package main.java.views;
 
+import java.awt.BorderLayout;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -11,5 +18,22 @@ public class TopView extends JPanel{
 	private JLabel srchLabel;
 	private JButton srchButton;
 	
-
+	public TopView(){
+		super(new FlowLayout(FlowLayout.CENTER));
+		this.setMaximumSize(new Dimension(1800,200));
+		searchField=new JTextField(50);
+		srchLabel=new JLabel("Type keywords here");
+		srchButton=new JButton();
+		srchButton.setText("Search");
+		srchLabel.setCursor(Cursor.getDefaultCursor());
+	    searchField.add(srchLabel, BorderLayout.LINE_END);
+	    srchLabel.addMouseListener(new MouseAdapter() {
+	        @Override
+	        public void mouseClicked(MouseEvent e) {
+	            searchField.setText("");
+	        }
+	    });
+		this.add(searchField);
+		this.add(srchButton);
+	}
 }
