68c2bbaef29b1a545e7af3c8464f310bfc5b534c
VZ <valentinazelayam@gmail.com>
2016-12-12 10:49:13 +0100

    Added some classes for the first working prototype and added ressources file for icons and Youtube API

--- src/main/java/app/UserInterface.java
+++ src/main/java/app/UserInterface.java
@@ -1,5 +1,34 @@
 package main.java.app;
 
-public class UserInterface {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import main.java.views.*;
+
+public class UserInterface extends JFrame {
+	
+	CentralView center;
+	
+	JPanel mainPanel;
+	
+	public UserInterface(){
+		super("A Youtube Copycat");
+		center=new CentralView();
+		mainPanel=new JPanel();
+		this.setPreferredSize(new Dimension(1500,1000));
+		mainPanel.setLayout(new BorderLayout());
+		mainPanel.add(center,BorderLayout.CENTER);
+		this.add(mainPanel);
+
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pack();
+		setVisible(true);
+	}
+	
+	
 
 }
---
+++ src/main/java/views/TopView.java
@@ -0,0 +1,15 @@
+package main.java.views;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class TopView extends JPanel{
+	
+	private JTextField searchField;
+	private JLabel srchLabel;
+	private JButton srchButton;
+	
+
+}
