19a9429f390d590ba8f83bcd678a234419d1bb5b
GTimothy <timothy.garwood@protonmail.com>
2016-12-13 14:48:57 +0100

    Completion of readme, correction of pom.xml

--- src/main/java/views/VideoTab.java
+++ src/main/java/views/VideoTab.java
@@ -1,14 +1,18 @@
 package views;
 
+import java.awt.BorderLayout;
 import java.awt.CardLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class VideoTab extends JPanel{
 	
 	private VideoPlayer videoPlayer;
 	private ImageIcon videoIcon;
+	private JPanel videoDetails;
 	
 	public ImageIcon getVideoIcon() {
 		return videoIcon;
@@ -20,14 +24,17 @@ public class VideoTab extends JPanel{
 
 	public VideoTab(){
 		super();
-		this.setLayout(new CardLayout());
+		this.setLayout(new BorderLayout());
 		String vidIconPath="src/main/resources/icons/video.png";
 		videoIcon = new ImageIcon(vidIconPath);
 		videoPlayer=new VideoPlayer();
 		this.add(videoPlayer);
-
-		
+		videoDetails = new JPanel();
+		videoDetails.setLayout(new BoxLayout(videoDetails, BoxLayout.Y_AXIS));
+		this.add(videoDetails,BorderLayout.SOUTH);
 	}
+	
+	
 	
 	public void playVideo(String url) throws InterruptedException{
 		videoPlayer.play(url);
