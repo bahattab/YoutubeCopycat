9e60229ba2e4ac02f30f486fe62fc958a970338a
VZ <valentinazelayam@gmail.com>
2016-12-14 18:06:49 +0100

    Changed home message

--- src/main/java/views/Home.java
+++ src/main/java/views/Home.java
@@ -1,20 +1,103 @@
 package views;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.net.URL;
+
+import javax.swing.Box;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Home extends JPanel{
 
-
+	JPanel mainPanel;
+	JPanel descriptPanel;
+	Box featuresPanel;
+	
+	
 	public Home(){
-		String message = "A video is running by default. To use this application, you can : "
-				+ "\n- Do a research with some key words by cliking on the search button\n "
-				+ "    - Once this search shows some results in the tab 'Search', you can click on a result to load the video in the Video Tab.\n"
-				+ "    - You can do as many search as you want, but the results will be replaced\n"
-				+ "- See some statictics by clicking on the graph icon"
-				+ "  \n- Add videos at your playlist by clicking on the add button\n"
-				+ "- Remove videos from your playlist by clicking on the remove button";
-		JOptionPane.showMessageDialog(null, message, "Use conditions",1);
+		super();
+		mainPanel=new JPanel(new BorderLayout());
+		descriptPanel=new JPanel(new GridLayout(5,1));
+		featuresPanel=Box.createHorizontalBox();
+		JLabel descript=new JLabel("Bienvenu sur Youtube Copycat, une application Java qui permet de visionner des videos depuis YouTube et depuis votre ordinateur ! ");
+		JLabel f=new JLabel("Cette application a les fonctionnalités suivantes :");
+		JLabel l1=new JLabel("");
+		
+		descriptPanel.add(descript);
+		descriptPanel.add(f);
+		
+		JPanel h=new JPanel();
+		JPanel s=new JPanel();
+		JPanel c=new JPanel();
+		JPanel st=new JPanel();
+		JPanel con=new JPanel();
+		JPanel most=new JPanel();
+		
+		
+		URL home=LeftSideBarView.class.getResource("/icons/home.png");
+		URL search=LeftSideBarView.class.getResource("/icons/search.png");
+		URL connect=LeftSideBarView.class.getResource("/icons/local.png");
+		URL stats=LeftSideBarView.class.getResource("/icons/stats.png");
+		URL connexes= LeftSideBarView.class.getResource("/icons/connexes.png");
+		URL mostWatched=LeftSideBarView.class.getResource("/icons/popular_today.png");
+		
+		ImageIcon icon1 = new ImageIcon(home);
+		ImageIcon icon2 = new ImageIcon(search);
+		ImageIcon icon3 = new ImageIcon(connect);
+		ImageIcon icon4 = new ImageIcon(stats);
+		ImageIcon icon5 = new ImageIcon(connexes);
+		ImageIcon icon6 = new ImageIcon(mostWatched);
+		
+		JLabel hLabel=new JLabel(icon1);
+		JLabel sLabel=new JLabel(icon2);
+		JLabel cLabel=new JLabel(icon3);
+		JLabel stLabel=new JLabel(icon4);
+		JLabel conLabel=new JLabel(icon5);
+		JLabel mostLabel=new JLabel(icon6);
+	
+		
+		JLabel hLabel2=new JLabel("Accéder à cette page d'accueil");
+		JLabel sLabel2=new JLabel("Chercher et ajouter une video de votre ordinateur sur la playlist courante");
+		JLabel cLabel2=new JLabel("Passer en mode connecté");
+		JLabel stLabel2=new JLabel("Consulter les statistiques de YouTube");
+		JLabel conLabel2=new JLabel("Afficher les suggestions de vidéos en rapport avec votre playlist");
+		JLabel mostLabel2=new JLabel("Afficher les vidéos les plus visionnées sur YouTube aujourd'hui");
+		
+		h.add(hLabel);
+		h.add(hLabel2);
+		
+		s.add(sLabel);
+		s.add(sLabel2);
+		
+		c.add(cLabel);
+		c.add(cLabel2);
+		
+		st.add(stLabel);
+		st.add(stLabel2);
+		
+		con.add(conLabel);
+		con.add(conLabel2);
+		
+		most.add(conLabel);
+		most.add(conLabel2);
+		
+		Box vbox= Box.createVerticalBox();
+		
+		vbox.add(h);
+		vbox.add(s);
+		vbox.add(c);
+		vbox.add(st);
+		vbox.add(con);
+		vbox.add(most);
+		
+		featuresPanel.add(vbox);
+		mainPanel.add(descriptPanel,BorderLayout.NORTH);
+		mainPanel.add(featuresPanel,BorderLayout.SOUTH);
+		
+		JOptionPane.showMessageDialog(null, mainPanel, "Welcome to Youtube Copycat !",1);
 	}
 	
 }
