50ced81c50c6c55c479271a2692074e344f9ee9e
VZ <valentinazelayam@gmail.com>
2016-12-14 14:44:26 +0100

    Added two buttons and corrected acces path to images so that it works as a jar

--- src/main/java/app/YoutubeCopycat.java
+++ src/main/java/app/YoutubeCopycat.java
@@ -10,7 +10,7 @@ public class YoutubeCopycat {
 		UserInterface ui=new UserInterface(app);
 		app.setUi(ui);
 		try{
-			ui.getCenter().getVideoTab().playVideo("https://www.youtube.com/watch?v=cggl4WN77Mw");
+			ui.getCenter().getVideoTab().playVideo("https://www.youtube.com/watch?v=RtRb51L3hPQ");
 			
 		}catch (InterruptedException interrupt){
 			System.out.println("playing was interrupted");
--- src/main/java/views/LeftSideBarView.java
+++ src/main/java/views/LeftSideBarView.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.net.URL;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -21,10 +22,20 @@ public class LeftSideBarView extends JPanel{
 	public LeftSideBarView(final AppController app){
 		this.setSize(100,800);
 		this.setLayout(new GridLayout(10,1));	
-		ImageIcon icon1 = new ImageIcon("src/main/resources/icons/home.png");
-		ImageIcon icon2 = new ImageIcon("src/main/resources/icons/search.png");
-		ImageIcon icon3 = new ImageIcon("src/main/resources/icons/local.png");
-		ImageIcon icon4 = new ImageIcon("src/main/resources/icons/stats.png");
+		
+		URL home=LeftSideBarView.class.getResource("/icons/home.png");
+		URL search=LeftSideBarView.class.getResource("/icons/search.png");
+		URL local=LeftSideBarView.class.getResource("/icons/local.png");
+		URL stats=LeftSideBarView.class.getResource("/icons/stats.png");
+		URL connexes= LeftSideBarView.class.getResource("/icons/connexes.png");
+		URL mostWatched=LeftSideBarView.class.getResource("/icons/popular_today.png");
+		
+		ImageIcon icon1 = new ImageIcon(home);
+		ImageIcon icon2 = new ImageIcon(search);
+		ImageIcon icon3 = new ImageIcon(local);
+		ImageIcon icon4 = new ImageIcon(stats);
+		ImageIcon icon5 = new ImageIcon(connexes);
+		ImageIcon icon6 = new ImageIcon(mostWatched);
 		JButton b1=new JButton(icon1);
 		b1.setMaximumSize(new Dimension(80,30));
 		JButton b2=new JButton(icon2);
@@ -32,7 +43,12 @@ public class LeftSideBarView extends JPanel{
 		JButton b3=new JButton(icon3);
 		b3.setMaximumSize(new Dimension(80,30));
 		JButton b4=new JButton(icon4);
-		b3.setMaximumSize(new Dimension(80,30));
+		b4.setMaximumSize(new Dimension(80,30));
+		JButton b5=new JButton(icon5);
+		b5.setMaximumSize(new Dimension(80,30));
+		JButton b6=new JButton(icon6);
+		b6.setMaximumSize(new Dimension(80,30));
+		
 		b1.addMouseListener(new MouseAdapter(){
 	    	@Override
 	    	public void mousePressed(MouseEvent e){
@@ -43,6 +59,8 @@ public class LeftSideBarView extends JPanel{
 		this.add(b2);
 		this.add(b3);
 		this.add(b4);
+		this.add(b5);
+		this.add(b6);
 		//this.add(Box.createHorizontalGlue());
 	}
 
